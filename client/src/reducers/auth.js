ce33664f0524aeed7e2c9a170005be72d936aa6e
yangluo0901 <yangluo0901@gmail.com>
2020-08-06 11:48:01 -0700

    so confused

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,32 +1,34 @@
 import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/constants";
 
 const initialState = {
-    token: localStorage.getItem("token"),
-    user: null,
-    finishLoad: false,
-    isAuthenticated: false,
+	token: localStorage.getItem("token"),
+	user: null,
+	finishLoad: false,
+	isAuthenticated: false,
 };
 
 const auth = (state = initialState, action) => {
-    const { type, payload } = action;
-    switch (type) {
-        case REGISTER_SUCCESS:
-            localStorage.setItem("token", payload.token);
-            return {
-                ...state,
-                ...payload,
-                finishLoad: true,
-                isAuthenticated: true,
-            };
-        case REGISTER_FAIL:
-            localStorage.removeItem("token");
-            return {
-                ...state,
-                token: null,
-                isAuthenticated: false,
-                finishLoad: true,
-            };
-        default:
-            return state;
-    }
+	const { type, payload } = action;
+	switch (type) {
+		case REGISTER_SUCCESS:
+			localStorage.setItem("token", payload.token);
+			return {
+				...state,
+				...payload,
+				finishLoad: true,
+				isAuthenticated: true,
+			};
+		case REGISTER_FAIL:
+			localStorage.removeItem("token");
+			return {
+				...state,
+				token: null,
+				isAuthenticated: false,
+				finishLoad: true,
+			};
+		default:
+			return state;
+	}
 };
+
+export default auth;
